7c583d149652a4c4fe5df4507acd1152f11be49a
OrestTudai <tudayorest3@gmail.com>
2025-11-20 21:59:19 +0200

    added small changes

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -19,8 +19,8 @@ export class EmailService {
     `;
 
     await this.resendService.send({
-      from: 'Invest Union <onboarding@resend.dev>',
-      to: 'vorozhbytr21@gmail.com',
+      from: `${name}, ${companyName} <onboarding@resend.dev>`,
+      to: 'invest-union@ukr.net',
       subject: companyName,
       html,
     });
