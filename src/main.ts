24cb8fb954af018f460a9c3fc88f93a82e58b0e0
OrestTudai <tudayorest3@gmail.com>
2025-11-22 23:51:57 +0200

    fixed cors policy

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://invest-union.com',
+      'https://www.invest-union.com',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
